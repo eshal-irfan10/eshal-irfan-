e444fd1e4fe24eb501c1fa3a950d4fc32119e1a5
Eshal Irfan <155638363+eshal-irfan10@users.noreply.github.com>
2024-06-19 05:34:55 -0700

    Add files via upload

---
+++ 2/script.js
@@ -0,0 +1,18 @@
+let username
+let myname ="Eshal Irfan";
+console.log(myname);
+let message = "hello world";
+console.log(message);
+alert("Welcome...to \n this  website")
+alert("Jhone Deo")
+alert("15 years old")
+alert("Certified Mobile Application Development" )
+alert("Pizza \n Pizz \n Piz \n Pi \n P")
+let email = "example@example.com"
+console.log(email);
+alert("My email address  is example@example.com ")
+let book = "A smater way to learn ";
+alert("I am trying to learn Javascript from A smater way to learn" ) ;
+document.write("Yah! I can write HTML content through Javascript")
+alert("------------------------------------END------------------------------------")
+// 13,15,20
